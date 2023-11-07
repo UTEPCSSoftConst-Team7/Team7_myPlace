e3fa8a2413fe382cbed464b07aaa4fd8b0313c7b
igaytan1 <igaytan1@miners.utep.edu>
2023-11-06 19:26:04 -0700

    Created main page components

---
+++ my-place-web-app/src/app/messaging/messaging.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-messaging',
+  templateUrl: './messaging.component.html',
+  styleUrls: ['./messaging.component.css']
+})
+export class MessagingComponent {
+
+}
