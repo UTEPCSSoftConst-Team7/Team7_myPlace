1c7dc623f15ed8271732646a3e9f5e6822ae84ac
mvdiazdele <mvdiazdele@miners.utep.edu>
2023-11-07 16:21:05 -0700

    quick changes to user profile

--- my-place-web-app/src/app/user/user.component.ts
+++ my-place-web-app/src/app/user/user.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
+
+
 export class UserComponent {
 
 }
