acb53799d4c4110f21e5372a25fafdf194536d6d
nicoJ115 <najara1@miners.utep.edu>
2023-11-08 16:40:16 -0700

    Finish everything for the User Page other then the search bar and messages

---
+++ my-place-web-app/src/app/my-service.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MyServiceService {
+
+  constructor() { }
+
+  setItem(key: string, value: any): void {
+    localStorage.setItem(key, JSON.stringify(value));
+  }
+
+  getItem(key: string): any {
+    const item = localStorage.getItem(key);
+    return item ? JSON.parse(item) : null;
+  }
+
+  removeItem(key: string): void {
+    localStorage.removeItem(key);
+  }
+
+  clear(): void {
+    localStorage.clear();
+  }
+  
+}
--- my-place-web-app/src/app/user/user.component.ts
+++ my-place-web-app/src/app/user/user.component.ts
@@ -1,11 +1,46 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 import { SearchBarComponent } from '../search-bar/search-bar.component';
+import { User } from '../User';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent {
+export class UserComponent implements OnInit {
+  Friend: User ={
+    username: 'NicoJ115',
+    password: 'adsfa',
+    email: 'sdafa'
+  }
+  ProfileUser: User= {
+    username: 'isaba3lla',
+    password: 'isaba3llaPass',
+    email: 'isaba3lla@example.com',
+    bio: 'Art and design lover',
+    profilePicture: '',
+    blogPosts: ["Painting progress","Coding my project","Going to Camp Flogna","Ban for Ban who work at MaterCard."],
+    friends: [this.Friend],
+  };
+
+
+
+  ngOnInit() {
+    if (this.grabUser()!= null){
+      this.ProfileUser.profilePicture=this.grabUser()
+    }
+    
+  }
+
+  grabUser(){
+   // this.ProfileUser= localStorage.getItem('profileUser')
+   var imageUrl = "https://static.vecteezy.com/system/resources/thumbnails/002/534/006/small/social-media-chatting-online-blank-profile-picture-head-and-body-icon-people-standing-icon-grey-background-free-vector.jpg"
+   localStorage.setItem('ProfilePic',imageUrl)
+   const profilePictureUrl = localStorage.getItem('profilePicture');
+   if (profilePictureUrl!=null || profilePictureUrl!=undefined){
+    return profilePictureUrl
+   }
+   return ""
+  }
 
 }
