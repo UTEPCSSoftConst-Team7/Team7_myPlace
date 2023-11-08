91c8013264b9c16e5334e54f3723b11f99b15544
Sosa714 <tsosa592@gmail.com>
2023-11-08 16:01:49 -0700

    Created initial iteration for messaging page

--- my-place-web-app/src/app/messaging/messaging.component.ts
+++ my-place-web-app/src/app/messaging/messaging.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-messaging',
   templateUrl: './messaging.component.html',
   styleUrls: ['./messaging.component.css']
 })
-export class MessagingComponent {
+export class MessagingComponent implements OnInit {
+  // Define the messages property as an array
+  messages = [
+    { username: 'User1', text: 'Hello, how are you?', fromMe: true },
+    { username: 'User2', text: 'I am fine, thanks! And you?', fromMe: false },
+    // ... other messages
+  ];
+  
 
+  constructor() { }
+
+  ngOnInit(): void {
+    // Initialization logic if needed
+  }
+
+  // Other methods and logic for your component
 }
+
