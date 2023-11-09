cfe1687b580b309e0d80f8b860dee4340579b60f
nicoJ115 <najara1@miners.utep.edu>
2023-11-08 17:30:52 -0700

    Change messaging.json

--- my-place-web-app/src/app/user/user.component.ts
+++ my-place-web-app/src/app/user/user.component.ts
@@ -26,21 +26,15 @@ export class UserComponent implements OnInit {
 
 
   ngOnInit() {
-    if (this.grabUser()!= null){
-      this.ProfileUser.profilePicture=this.grabUser()
-    }
+    this.grabUser()
     
   }
 
   grabUser(){
-   // this.ProfileUser= localStorage.getItem('profileUser')
-   var imageUrl = "https://static.vecteezy.com/system/resources/thumbnails/002/534/006/small/social-media-chatting-online-blank-profile-picture-head-and-body-icon-people-standing-icon-grey-background-free-vector.jpg"
-   localStorage.setItem('ProfilePic',imageUrl)
-   const profilePictureUrl = localStorage.getItem('profilePicture');
-   if (profilePictureUrl!=null || profilePictureUrl!=undefined){
-    return profilePictureUrl
-   }
-   return ""
+  //  this.ProfileUser= localStorage.getItem('profileUser')
+   this.ProfileUser.profilePicture = "assert/"+this.ProfileUser.profilePicture
+   
+
   }
 
 }
