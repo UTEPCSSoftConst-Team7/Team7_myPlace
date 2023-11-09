0e1911817e2eb30a0d01e8623ebbd03a23282188
mvdiazdele <mvdiazdele@miners.utep.edu>
2023-11-09 16:57:48 -0700

    updated changes

--- my-place-web-app/src/app/search-bar/search-bar.component.ts
+++ my-place-web-app/src/app/search-bar/search-bar.component.ts
@@ -2,9 +2,9 @@ import {Component, OnInit} from '@angular/core';
 import {FormControl, FormsModule, NgModel, ReactiveFormsModule} from '@angular/forms';
 import {Observable} from 'rxjs';
 
-import { UserService } from '../user/user.service';
+import { UserService } from '../user.service';
 import { Router } from '@angular/router';
-import { User } from '../user/user.interface';
+import { User } from '../User';
 
 @Component({
   selector: 'app-search-bar',
--- my-place-web-app/src/app/user.service.ts
+++ my-place-web-app/src/app/user.service.ts
@@ -1,14 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { User } from './user.interface';
+import { User } from './User';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-  private usersUrl = 'assets/UserFile.json';
+  private usersUrl = 'assets/users.json';
 
   constructor(private http: HttpClient) { }
 
