907a605f72a21aa0057d0ae53c5f2702159168f2
eflores31 <esai.flores.88@gmail.com>
2023-11-08 18:57:59 -0700

    updated notification_class

--- my-place-flask-backend/notification_class.py
+++ my-place-flask-backend/notification_class.py
@@ -2,17 +2,43 @@
 import user_profile_class
 
 class Notification:
-    def __init__(self, notification_type, content, user_profile):
-        self.notification_type = notification_type
-        self.content = content
-        self.user_profile = user_profile
-        self.viewed = False
+    def __init__(self, notification_type, content, user_profile, viewed):
+        self._notification_type = notification_type
+        self._content = content
+        self._user_profile = user_profile
+        self._viewed = False
+
+    # getters and setters for Notification class
+    ################################
 
     def get_user(self):
-        self.user_profile = user_profile_class.get_information(self.user_profile)
+        return self._user_profile
+
+    def set_user(self, user):
+        self._user_profile = user_profile_class.get_information(self.user_profile)
+
+    def get_notification_type(self):
+        return self._notification_type
+
+    def set_notification_type(self, notification_type):
+        self._notification_type = notification_type
+
+    def get_content(self):
+        return self._content
+
+    def set_content(self, content):
+        self._content = content
+
+    def get_view(self):
+        return self._viewed
+
+    def set_view(self, view):
+        self._viewed = view
+
+    ##################################
 
     def view_notification(self):
-        self.viewed = True
+        self._viewed = True
 
     def generate_alert(self):
         # Generate a visual or audio alert for the user, e.g., displaying a pop-up notification
