8729e7c87dcf4eb574a100f6d7db25e2540abf1f
eflores31 <esai.flores.88@gmail.com>
2023-11-08 17:24:45 -0700

    updated direct_messaging.py

--- my-place-flask-backend/direct_messaging.py
+++ my-place-flask-backend/direct_messaging.py
@@ -1,20 +1,51 @@
+import user_profile_class
+from datetime import datetime
+
 # Direct Messaging:
 # Send Message
 # Receive message
-# Create/Add to/Remove from/Delete Group
+# Create/Add to/Remove from/Delete
 
 class Message:
     def __init__(self, sender, recipients, content, timestamp):
-        self.sender = sender
-        self.recipients = recipients
-        self.content = content
-        self.timestamp = timestamp
+        self._sender = sender
+        self._recipients = recipients
+        self._content = content # str
+        self._timestamp = timestamp
+
+    # getters and setters for Message class
+    ################################
+    def get_sender(self):
+        return self._sender
+
+    def set_sender(self, sender):
+        self._sender = sender
+
+    def get_recipients(self):
+        return self._recipients
+
+    def set_recipients(self, recipients):
+        self._recipients = recipients
+
+    def get_content(self):
+        return self._content
+
+    def set_content(self, content):
+        self._content = ""
+
+    def get_timestamp(self):
+        return self._timestamp
+
+    def set_timestamp(self, timestamp):
+        self._timestamp = timestamp
+
+    ################################
 
     def store_message(self, content, timestamp, database):
         # Implement message storage logic, e.g., storing the message in a database
         message_data = {
-            "sender": self.sender,
-            "recipients": self.recipients,
+            "sender": self._sender,
+            "recipients": self._recipients,
             "content": content,
             "timestamp": timestamp,
         }
@@ -24,21 +55,21 @@ class Message:
         # Implement message delivery logic, e.g., sending the message to the recipient(s)
         for recipient in recipients:
             # Send the message to the recipient, e.g., through a socket connection
-            recipient.receive_message(self.sender, content, timestamp)
+            recipient.receive_message(self._sender, content, timestamp)
 
     def maintain_message_history(self, timestamp, database):
         # Implement logic to maintain a record of message histories, e.g., storing in user's message history
         message_data = {
-            "sender": self.sender,
-            "content": self.content,
+            "sender": self._sender,
+            "content": self._content,
             "timestamp": timestamp,
         }
-        self.sender.add_message_history(message_data)
+        self._sender.add_message_history(message_data)
 
     def send_message(self, database):
-        self.store_message(self.content, self.timestamp, database)
-        self.deliver_message(self.recipients, self.content, self.timestamp)
-        self.maintain_message_history(self.timestamp, database)
+        self.store_message(self._content, self._timestamp, database)
+        self.deliver_message(self._recipients, self._content, self._timestamp)
+        self.maintain_message_history(self._timestamp, database)
 
     @staticmethod
     def receive_message(sender, content, timestamp):
@@ -47,6 +78,6 @@ class Message:
 
     def delete_message_history(self, content):
         # Implement logic to delete a message from the sender's and recipient(s') message histories
-        self.sender.delete_message(content)
-        for recipient in self.recipients:
+        self._sender.delete_message(content)
+        for recipient in self._recipients:
             recipient.delete_message(content)
