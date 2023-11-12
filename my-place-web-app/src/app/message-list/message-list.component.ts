ccf827f55e75874dfe3685191c2983f36569b377
Sosa714 <tsosa592@gmail.com>
2023-11-12 16:55:00 -0700

    Started on front-end to message-list-page

--- my-place-web-app/src/app/message-list/message-list.component.ts
+++ my-place-web-app/src/app/message-list/message-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../user.service'; // Adjust the path as necessary
 
 @Component({
   selector: 'app-message-list',
@@ -6,10 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./message-list.component.css']
 })
 export class MessageListComponent implements OnInit {
+  messageGroups: any[] = []; // Define the property here
 
-  constructor() { }
+  constructor(private userService: UserService) { }
 
   ngOnInit(): void {
+    this.loadMessageGroups();
   }
 
+  loadMessageGroups() {
+    // Logic to load and process message groups
+    this.userService.getMessages().subscribe(messages => {
+      // Process the messages to create message groups
+      // This is a placeholder logic. You need to implement the actual logic
+      // to group messages and extract the necessary details.
+      this.messageGroups = this.processMessages(messages);
+    });
+  }
+
+  processMessages(messages: any[]): any[] {
+    // Implement the logic to process messages and return an array of message groups
+    // Each group should have 'participants' and 'lastMessage' properties
+    // This is just a placeholder function. You need to fill in the logic.
+    return [];
+  }
 }
